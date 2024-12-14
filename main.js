07de308e2f10f4af60c7c87eabdca1f7bae3cc22
MOHY2013 <haammaaddaa13@gmail.com>
2024-12-14 22:05:00 +0200

    1.0.0

---
+++ main.js
@@ -0,0 +1,41 @@
+console.log("omer");
+console.log(34);
+
+console.log(typeof["mohamed","mohamed","mohamed",1]);
+console.log(typeof(1));
+console.log(typeof("mmmm"));
+console.log(["mohy","mohy",1]);
+let y;
+let d=89;
+ let k=77;
+ y=d-k;
+ console.log(y);
+ let o;
+let l=3;
+ let m=3;
+ o=l*m;
+ console.log(o);
+ let nn=6;
+ if(t==5){
+    console.log("treu");
+ }
+ else if(t<=5){
+console.log(456);
+ }
+ else{
+    console.log("Noooooooooooo");
+ }
+ let tyu=1;
+switch(tyu){
+case 2:
+    console.log(5);
+    case 3:
+        console.log(10) ;
+        case 1:
+             console.log("mmmooohhhyyy");
+}
+let tg=0;
+while(tg<=5){
+    console.log(tg);
+}
+ 
